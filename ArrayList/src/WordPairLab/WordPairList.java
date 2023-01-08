44e99675932071824a37d82ef6c69ef80bc28edb
xiao_mao <laurenli045@gmail.com>
2023-01-08 13:46:39 -0500

    creates an arraylist of WordPair objects and finds the number of pairs

---
+++ ArrayList/src/WordPairLab/WordPairList.java
@@ -0,0 +1,28 @@
+package WordPairLab;
+import java.util.ArrayList;
+
+public class WordPairList {
+    private ArrayList<WordPair> allPairs= new ArrayList<WordPair>();
+    public WordPairList (String [] wrds)
+    {
+        for(int i=0; i<wrds.length; i++) {
+            for(int j=i+1; j<wrds.length; j++){
+                allPairs.add(new WordPair (wrds[i], wrds[j]));
+            }
+        }
+    }
+
+    public int numMatches(){
+        int counter=0;
+        for (int i=0; i<allPairs.size(); i++) {
+            String first= allPairs.get(i).getFirst();
+            String second= allPairs.get(i).getSecond();
+
+            if(first.equals(second)==true) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+}
