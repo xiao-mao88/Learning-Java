3434d85b9855062626cf82084a2872b58491a8f3
xiao_mao <laurenli045@gmail.com>
2023-01-17 23:30:33 -0500

    very simple, just prints out a few quotes of famous people

---
+++ Chapter1/src/FamousQuotes.java
@@ -0,0 +1,12 @@
+public class FamousQuotes {
+    public static void main(String[] args) {
+        System.out.println("''Have no fear of perfection, you'll never reach it.''");
+        System.out.println("~Salvador Dali\n");
+
+        System.out.println("''Intellectual growth should commence at growth and only cease at death''");
+        System.out.println("~Albert Einstein\n");
+
+        System.out.println("''We can complain because rose bushes have thorns, or rejoice because thorn bushes have roses.''");
+        System.out.println("~Abraham Lincoln");
+    }
+}
